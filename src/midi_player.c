9b0bdd4e2c46adef9a0f28908a505e0e9cfac9bb
Furkan <bilalyigit594@gmail.com>
2025-11-25 11:19:54 +0300

    [UPDATE]: Improved audio quality by updating synth settings

--- src/midi_player.c
+++ src/midi_player.c
@@ -2,33 +2,56 @@
 #include <fluidsynth.h>
 
 int play_midi_muted(const char *soundfont, const char *midi_file) {
-    fluid_settings_t* settings;
-    fluid_synth_t* synth;
-    fluid_player_t* player;
-    fluid_audio_driver_t* adriver;
+    int res = 0;
+    
+    fluid_settings_t* settings = new_fluid_settings();
 
-    settings = new_fluid_settings();
-    // Use the default audio driver
+    // Default is 0.2. increased it to 2.0 to gain volume
+    fluid_settings_setnum(settings, "synth.gain", 1.0);
+
+    // Increasing the period size gives the CPU more time to process audio.
+    // Default period-size is 64.We'll increase it to 1024. This adds a tiny bit of latency (milliseconds), but fixes the crackling.
+    fluid_settings_setint(settings, "audio.period-size", 1024);
+    fluid_settings_setint(settings, "audio.periods", 2);
+    fluid_settings_setnum(settings, "synth.sample-rate", 44100);
+
     fluid_settings_setstr(settings, "audio.driver", "pulseaudio");
-    synth = new_fluid_synth(settings);
-    adriver = new_fluid_audio_driver(settings, synth);
 
+    fluid_synth_t* synth = new_fluid_synth(settings);
+    
+    if (synth == NULL) {
+        fprintf(stderr, "Error: Failed to create synthesizer.\n");
+        delete_fluid_settings(settings);
+        return 1;
+    }
 
+    fluid_audio_driver_t* adriver = new_fluid_audio_driver(settings, synth);
+    
+    if (adriver == NULL) {
+        fprintf(stderr, "Error: Failed to create audio driver.\n");
+        delete_fluid_synth(synth);
+        delete_fluid_settings(settings);
+        return 1;
+    }
+
+    // LOAD THE SOUNDFONT
     if (fluid_synth_sfload(synth, soundfont, 1) == FLUID_FAILED) {
         printf("Error: Failed to load the SoundFont file: %s\n", soundfont);
+        res = 1;
         goto cleanup;
     }
     printf("🎹 SoundFont loaded successfully.\n");
 
-
-    player = new_fluid_player(synth);
+    // CREATE THE MIDI PLAYER
+    fluid_player_t* player = new_fluid_player(synth);
     
-    // Add the MIDI file to the player's playlist
     if (fluid_player_add(player, midi_file) == FLUID_FAILED) {
         printf("Error: Failed to add MIDI file: %s\n", midi_file);
+        res = 1;
+        delete_fluid_player(player);
         goto cleanup;
     }
-    printf("🎵 MIDI file added to playlist.\n");
+     printf("🎵 MIDI file added to playlist.\n");
 
     printf("Playing... Press Enter to stop.\n");
     fluid_player_play(player);
@@ -45,6 +68,5 @@ cleanup:
     delete_fluid_player(player);
     delete_fluid_synth(synth);
     delete_fluid_settings(settings);
-
-    return 0;
+    return res;
 }
