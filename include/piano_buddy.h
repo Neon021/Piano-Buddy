b5f5e23717a937f169eed6666de81a955513b0cc
Furkan <bilalyigit594@gmail.com>
2025-12-31 11:01:43 +0300

    [UPDATE]: Removed old files related to .mid process and replaced them with processor_bridge

--- include/piano_buddy.h
+++ include/piano_buddy.h
@@ -1,5 +1,3 @@
-#include <stdbool.h>
-
 #ifndef PIANO_BUDDY_H
 #define PIANO_BUDDY_H
 
@@ -9,13 +7,7 @@ int record_audio(const char *filename, int seconds);
 // recognizer.c
 char* identify_song(const char *filename);
 
-// scraper_bridge.c
-char* get_midi_url_from_python(const char *query);
-
-// downloader.c
-int download_file(const char *url, const char *output_filename);
-
-// midi_player.c
-int play_midi_muted(const char *soundfont, const char *midi_file, volatile bool *should_stop);
+// processor_bridge.c
+int process_song_with_python(const char* song_name);
 
 #endif
